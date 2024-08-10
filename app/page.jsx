6000a4031a0f183f86a410d1d7c8922726f4416e
AbhishekNavgan95 <navganabhishek90@gmail.com>
2024-08-10 20:43:58 +0530

    trigger deploy

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Feed from "@components/Feed"
 
 const Home = () => { 
-    return (
+    return ( 
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">Discover and share
                 <br className="max-md:hidden" />
