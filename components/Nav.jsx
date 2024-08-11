203c9ef65fbf628a434220d28b76c4eb6c646fd7
AbhishekNavgan95 <navganabhishek90@gmail.com>
2024-08-11 11:21:48 +0530

    added session log

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
     const { data: session } = useSession();
+    console.log("user session : ", session);
     const [providers, setProviders] = useState(null);
     const [toggleDropDown, setToggleDropDown] = useState(false);
 
